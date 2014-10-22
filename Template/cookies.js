7fe1228772784fa78523a74e079f52fab1fbfa9f
as2466967 <safaie.azadeh@gmail.com>
2014-10-22 15:48:41 -0700

    Signed-off-by: as2466967 <safaie.azadeh@gmail.com>

---
+++ Template/cookies.js
@@ -0,0 +1,44 @@
+
+function getForm(url){
+	var info=url.split("?");
+	var nameValuePairs=info[1].split("&");
+	var $_GET = new Object;
+	for(var i=0;i<nameValuePairs.length;i++){
+		var map=nameValuePairs[i].split("=");
+		var name=map[0];
+		var value=map[1];
+		$_GET[name]=value;
+	}
+	return $_GET;
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
---
+++ Template/functionSample.js
@@ -0,0 +1,9 @@
+
+function ranNum( ){
+	var num=(Math.random()*5)+1;
+	return num;
+}
+function user(name){
+	document.write(name);
+}
+
